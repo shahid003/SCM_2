9866a39d2f7e89e90e1552e18bfa7d6faffb2640
shivi9393 <146976694+shivi9393@users.noreply.github.com>
2024-02-22 11:01:09 +0530

    Update shubham.c
    kaka is here

--- shubham.c
+++ shubham.c
@@ -2,4 +2,14 @@
 void main()
 {
   printf("hello");
+  printf("Oh sadeyan ta lekhan ch black likheya\n
+Tere favourite pink-pink baliye\n
+Sidhu Moose Wala, Moose Wala naam sunida\n
+Maame gang'an naal jorhde aa link baliye\n
+Oh sadeyan ta lekhan ch black likheya\n
+Tere favourite pink-pink baliye\n
+Sidhu Moose Wala, Moose Wala naam sunida\n
+Maame gang'an naal jorhde aa link baliye\n
+Pyaar nal jo rehnda ohnu rakh laine aa\n
+Karda jo tadi ohnu chak laine aan\n);
 }
